dd95eb708451a7a16b77820d975110a10d9abc16
Arturo Volpe <arturovolpe@gmail.com>
2016-01-22 10:16:37 -0300

    Eliminación de funciones innecesarias
    Se borran el controlador del modelo, y funciones que no sirven.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,39 +8,6 @@ angular.module('starter.controllers', [])
     else return '';
   };
 })
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
 
 .controller('FrasesCtrl', function($scope, FrasesService) {
 
@@ -56,12 +23,6 @@ angular.module('starter.controllers', [])
 
   $scope.visualizarFrase = function(){
     $scope.frase = getRandomFrase();
-  };
-
-  $scope.doRefresh = function(){
-    document.getElementById("frase1").style.visibility = "hidden";
-    $scope.$broadcast('scroll.refreshComplete');
-    $scope.$apply();
   };
 
 })
